e9389b6f67a3ca0bbbd1b4ef5e2b5d668b8765b6
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2012-01-21 17:40:07 +0000

    added cent56 and sl61 veewee definitions

---
+++ veewee/definitions/centos56/postinstall.sh
@@ -0,0 +1,49 @@
+#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
+
+date > /etc/vagrant_box_build_time
+
+fail()
+{
+  echo "FATAL: $*"
+  exit 1
+}
+
+#kernel source is needed for vbox additions
+yum -y install gcc bzip2 make 
+#yum -y update
+#yum -y upgrade
+
+#yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y clean all
+
+#Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+#Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sed -i "s/^\(.*env_keep = \"\)/\1PATH /" /etc/sudoers
+
+yum -y remove gcc make kernel-devel krb5-devel libselinux-devel libsepol-devel keyutils-libs-devellibtermcap-devel glibc-devel libstdc++-devel e2fsprogs-devel pcsc-lite iptables-ipv6 yum-updatesd sendmail
+
+chkconfig gpm off
+chkconfig autofs off
+chkconfig iscsid off
+
+#poweroff -h
+
+exit
